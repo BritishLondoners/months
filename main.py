83a1278527b7975c5db9c2a46333616880cc1611
BritishLondoners <adrikadasaadha@gmail.com>
2026-04-20 23:39:05 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+def display_months():
+    # List of all months
+    months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ]
+
+    # Print each month
+    for month in months:
+        print(month)
+
+# Run the program
+display_months()
